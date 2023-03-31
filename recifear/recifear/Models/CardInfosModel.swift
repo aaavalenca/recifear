7b0a35bd13a9f620b808d25783a8ea54cca9665b
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-03-31 17:02:18 -0300

    creating Folder Models

--- recifear/recifear/Models/CardInfosModel.swift
+++ recifear/recifear/Models/CardInfosModel.swift
@@ -5,11 +5,12 @@
 //  Created by Danielly Santos Lopes da Silva on 31/03/23.
 //
 
+
 import Foundation
 
 
 struct CardInfos : Identifiable {
-    //, Hashable 
+   
     var id: UUID
     var image: String
     var text: String
